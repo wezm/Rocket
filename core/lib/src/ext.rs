f83caf2d08cb409f0e4f3abe5d3a101e8e1580eb
Jeb Rosen <jeb@jebrosen.com>
2019-07-11 18:54:48 -0700

    Stream body data instead of buffering it.
    This requires some awkward channel and spawning work because
    Body might contain borrowed data.

--- core/lib/src/ext.rs
+++ core/lib/src/ext.rs
@@ -3,7 +3,10 @@ use std::pin::Pin;
 
 use futures::io::{AsyncRead, AsyncReadExt as _};
 use futures::future::{Future};
+use futures::stream::Stream;
 use futures::task::{Poll, Context};
+
+use crate::http::hyper::Chunk;
 
 // Based on std::io::Take, but for AsyncRead instead of Read
 pub struct Take<R>{
@@ -30,9 +33,43 @@ impl<R> AsyncRead for Take<R> where R: AsyncRead + Unpin {
     }
 }
 
+pub struct IntoChunkStream<R> {
+    inner: R,
+    buf_size: usize,
+    buffer: Vec<u8>,
+}
+
+// TODO.async: Verify correctness of this implementation.
+impl<R> Stream for IntoChunkStream<R>
+    where R: AsyncRead + Unpin
+{
+    type Item = Result<Chunk, io::Error>;
+
+    fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>>{
+        assert!(self.buffer.len() == self.buf_size);
+
+        let Self { ref mut inner, ref mut buffer, buf_size } = *self;
+
+        match Pin::new(inner).poll_read(cx, &mut buffer[..]) {
+            Poll::Pending => Poll::Pending,
+            Poll::Ready(Err(e)) => Poll::Ready(Some(Err(e))),
+            Poll::Ready(Ok(n)) if n == 0 => Poll::Ready(None),
+            Poll::Ready(Ok(n)) => {
+                let mut next = std::mem::replace(buffer, vec![0; buf_size]);
+                next.truncate(n);
+                Poll::Ready(Some(Ok(Chunk::from(next))))
+            }
+        }
+    }
+}
+
 pub trait AsyncReadExt: AsyncRead {
     fn take(self, limit: u64) -> Take<Self> where Self: Sized {
         Take { inner: self, limit }
+    }
+
+    fn into_chunk_stream(self, buf_size: usize) -> IntoChunkStream<Self> where Self: Sized {
+        IntoChunkStream { inner: self, buf_size, buffer: vec![0; buf_size] }
     }
 
     // TODO.async: Verify correctness of this implementation.
