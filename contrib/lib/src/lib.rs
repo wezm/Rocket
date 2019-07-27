2109235e012ed792160a59819f9aec236f1ae564
Jeb Rosen <jeb@jebrosen.com>
2019-07-27 09:15:23 -0700

    Update parts of contrib and associated tests and examples:
    * json
    * templates
    * helmet
    * databases
    * serve
    * examples/cookies
    * examples/handlebars_templates
    * examples/json
    * examples/session
    * examples/static_files
    * examples/tera_templates

--- contrib/lib/src/lib.rs
+++ contrib/lib/src/lib.rs
@@ -1,3 +1,4 @@
+#![feature(async_await)]
 #![doc(html_root_url = "https://api.rocket.rs/v0.5")]
 #![doc(html_favicon_url = "https://rocket.rs/v0.5/images/favicon.ico")]
 #![doc(html_logo_url = "https://rocket.rs/v0.5/images/logo-boxed.png")]
