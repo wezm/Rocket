ad457b4277196467ec37f863f6fa5fd2f667bf63
Sergio Benitez <sb@sergio.bz>
2020-06-21 05:44:12 -0700

    Make 'NamedFile' async. Fix 'Handler' trait.
    Previously, 'NamedFile::open()' called a synchronous I/O method. This
    commit changes it to instead use tokio's 'File' for async I/O.
    
    To allow this to change, the 'Handler' trait was fixed to enforce that
    the lifetime of '&self', the reference to the handler, outlives the
    incoming request. As a result, futures returned from a handler can hold
    a reference to 'self'.

--- contrib/lib/src/serve.rs
+++ contrib/lib/src/serve.rs
@@ -259,23 +259,26 @@ impl Into<Vec<Route>> for StaticFiles {
     }
 }
 
+async fn handle_dir<'r, P>(opt: Options, req: &'r Request<'_>, data: Data, path: P) -> Outcome<'r>
+    where P: AsRef<Path>
+{
+    if !opt.contains(Options::Index) {
+        return Outcome::forward(data);
+    }
+
+    let file = NamedFile::open(path.as_ref().join("index.html")).await.ok();
+    Outcome::from_or_forward(req, data, file)
+}
+
 impl Handler for StaticFiles {
-    fn handle<'r>(&self, req: &'r Request<'_>, data: Data) -> HandlerFuture<'r> {
-        fn handle_dir<'r>(opt: Options, r: &'r Request<'_>, d: Data, path: &Path) -> Outcome<'r> {
-            if !opt.contains(Options::Index) {
-                return Outcome::forward(d);
-            }
-
-            let file = NamedFile::open(path.join("index.html")).ok();
-            Outcome::from_or_forward(r, d, file)
-        }
-
+    fn handle<'r, 's: 'r>(&'s self, req: &'r Request<'_>, data: Data) -> HandlerFuture<'r> {
         // If this is not the route with segments, handle it only if the user
         // requested a handling of index files.
         let current_route = req.route().expect("route while handling");
         let is_segments_route = current_route.uri.path().ends_with(">");
         if !is_segments_route {
-            return handle_dir(self.options, req, data, &self.root).pin();
+            let result = handle_dir(self.options, req, data, &self.root);
+            return Box::pin(async move { result.await });
         }
 
         // Otherwise, we're handling segments. Get the segments as a `PathBuf`,
@@ -286,10 +289,12 @@ impl Handler for StaticFiles {
             .and_then(|segments| segments.into_path_buf(allow_dotfiles).ok())
             .map(|path| self.root.join(path));
 
-        match &path {
-            Some(path) if path.is_dir() => handle_dir(self.options, req, data, path),
-            Some(path) => Outcome::from_or_forward(req, data, NamedFile::open(path).ok()),
-            None => Outcome::forward(data),
-        }.pin()
+        Box::pin(async move {
+            match path {
+                Some(p) if p.is_dir() => handle_dir(self.options, req, data, p).await,
+                Some(p) => Outcome::from_or_forward(req, data, NamedFile::open(p).await.ok()),
+                None => Outcome::forward(data),
+            }
+        })
     }
 }
